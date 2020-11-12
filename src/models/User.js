a7d37ac5f13a21c887d674b1508982846785e8fb
Aldo Guasch <jhonandrs26@gmail.com>
2020-11-11 21:09:00 -0300

    Version Final Del modelo

--- src/models/User.js
+++ src/models/User.js
@@ -12,6 +12,16 @@ const UserSchema = new Schema({
 	token: { type: String },
 });
 
+UserSchema.methods.encryptPassword = async (password) => {
+	const salt = await bcrypt.genSalt(10);
+	const hash = bcrypt.hash(password, salt);
+
+	return hash;
+};
+
+UserSchema.methods.matchPassword = async function (password) {
+	return await bcrypt.compare(password, this.password);
+};
 
 
 module.exports = mongoose.model("User", UserSchema);
